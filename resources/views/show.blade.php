51689ab30ca73a529b3352a9f3c8124660b2eb70
Sayed-Nur-E-Newaz <miznewaz@gmail.com>
2023-01-23 15:58:40 +0600

    show file updated

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -7,29 +7,37 @@
         <div class="row justify-content-center">
             <div class="col col-md-4">
                 <h5>Image 1</h5>
-                <figure>                    
+                <figure>
+
                     <img src="{{ $image1 }}" alt="{{ $description }}" class="img-thumbnail">
-                </figure>   
+
+
+                </figure>
                 <p>{{ $description }}</p>
-                
-                
-                      
+
+
+
             </div>
             <div class="col col-md-4">
                 <h5>Image 2</h5>
                 <figure>
                     <img src="{{ $image2 }}" alt="{{ $description }}" class="img-thumbnail">
                 </figure>
-                <p>{{ $description }}</p>  
+                <p>{{ $description }}</p>
             </div>
             <hr>
-                <div class="d-flex justify-content-center">
-                    <button class="btn btn-secondary  btn-block me-3 loader" onclick="window.href.load()"> Generate Again </button>
-                    
-                    <a href="{{ route('welcome') }}" class="btn btn-dark btn-block ms-3" >Back to form</a>
-                </div>  
+            <div class="d-flex justify-content-center">
+                <button class="btn btn-secondary  btn-block me-3" onClick="window.location.reload()"> Generate Again
+                </button>
+
+                <a href="{{ route('welcome') }}" class="btn btn-dark btn-block ms-3">Back to form</a>
+            </div>
         </div>
     </div>
+
+
+
+
 </section>
 
 
@@ -37,4 +45,3 @@
 
 
 @endsection
-
